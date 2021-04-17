9867766a8a434de830b6953afbd8cce36aee8dcb
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-04-17 12:19:00 +0530

    Update devil.py

--- DaisyX/devil.py
+++ DaisyX/devil.py
@@ -2,6 +2,7 @@ BOT_TOKEN = "1455:AAE4qFS3nF-0Tn"
 API_ID = 69
 API_HASH = "e46b6c854d2bf580"
 PHONE_NUMBER = "+910000000000" # Need for Helper Userbot
+OWNER_ID = 1037581197
 MAIN_CHATS = [-1001216133474]  # NOTE These are the chats where main functions of bot will work, like music downloading etc.
 SUDO_USERS_ID = [1037581197, 1513257955] # Sudo users have full access to everythin, don't trust anyone
 LOG_GROUP_ID = -1001216133474
