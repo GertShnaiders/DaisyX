a1af34d986728dfaf788e8e5d14821584d62c3f1
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-04-17 12:33:07 +0530

    Update devil.py

--- DaisyX/devil.py
+++ DaisyX/devil.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "1455:AAE4qFS3nF-0Tn"
-API_ID = 69
-API_HASH = "e46b6c854d2bf580"
+API_ID = 2460843
+API_HASH = "43ad3b9bb7765b0867ad7b18e9259da6"
 PHONE_NUMBER = "+910000000000" # Need for Helper Userbot
 OWNER_ID = 1037581197
 MAIN_CHATS = [-1001216133474]  # NOTE These are the chats where main functions of bot will work, like music downloading etc.
